3dbab73a095dd28b51cfaa9c14797b0928306f02
Alexander Stehlik <alexander.stehlik@gmail.com>
2014-09-09 17:28:07 +0200

    [TASK] Cleanup ext_emconf.php file for upload

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,29 +14,9 @@ $EM_CONF[$_EXTKEY] = array(
 	'title' => 'HTML5 media kit',
 	'description' => 'Allows easy embedding of HTML5 video and audio elements.',
 	'category' => 'fe',
-	'shy' => 0,
-	'dependencies' => 'cms',
-	'conflicts' => '',
-	'priority' => '',
-	'loadOrder' => '',
-	'module' => '',
-	'state' => 'alpha',
-	'internal' => 1,
-	'uploadfolder' => 0,
-	'createDirs' => '',
-	'modify_tables' => '',
-	'clearCacheOnLoad' => 1,
-	'lockType' => 'S',
-	'author' => 'Alexander Stehlik',
-	'author_email' => 'alexander.stehlik.deleteme@gmail.com',
-	'author_company' => '',
-	'CGLcompliance' => '',
-	'CGLcompliance_note' => '',
-	'version' => '0.0.1',
-	'_md5_values_when_last_written' => '',
 	'constraints' => array(
 		'depends' => array(
-			'cms' => '6.2.1-6.2.99',
+			'typo3' => '6.2.1-6.2.99',
 			'extbase' => '',
 		),
 		'conflicts' => array(
@@ -44,8 +24,15 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'suggests' => array(
-	),
+	'loadOrder' => '',
+	'state' => 'stable',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'clearCacheOnLoad' => 1,
+	'author' => 'Alexander Stehlik',
+	'author_email' => 'alexander.stehlik.deleteme@gmail.com',
+	'author_company' => '',
+	'version' => '1.0.0',
 );
 
 ?>
