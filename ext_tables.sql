3a23472fcb2b1e4ff49ef8ea9c3478283bab8500
Alexander Stehlik <alexander.stehlik@gmail.com>
2019-03-10 18:48:12 +0100

    [!!!][TASK] Cleanup and adjust ext_tables.sql
    Remove all fields that are handled by TYPO3 automatically.
    
    Make integer columns unsigned. we do not need to store
    negative integers and some of them reference unsigned
    integeger columns (like parent_record or content_element).
    
    Reduce size of file reference fields. They only contain the
    count and not the ID reference and the count can not get greater
    than one.

--- ext_tables.sql
+++ ext_tables.sql
@@ -9,30 +9,16 @@ CREATE TABLE tt_content (
 # Table structure for table 'tx_html5mediakit_domain_model_mediacontentelement'
 #
 CREATE TABLE tx_html5mediakit_domain_model_media (
-	uid int(11) NOT NULL auto_increment,
-	pid int(11) DEFAULT '0' NOT NULL,
-	tstamp int(11) DEFAULT '0' NOT NULL,
-	crdate int(11) DEFAULT '0' NOT NULL,
-	cruser_id int(11) DEFAULT '0' NOT NULL,
-	deleted tinyint(4) DEFAULT '0' NOT NULL,
-
-	content_element int(11) DEFAULT '0' NOT NULL,
-	parent_record int(11) DEFAULT '0' NOT NULL,
+	content_element int(11) unsigned DEFAULT '0' NOT NULL,
+	parent_record int(11) unsigned DEFAULT '0' NOT NULL,
 	parent_table varchar(255) DEFAULT '' NOT NULL,
 
 	type varchar(100) DEFAULT '' NOT NULL,
 	caption varchar(255) DEFAULT '' NOT NULL,
 	description text NOT NULL,
-	mp3 int(11) DEFAULT '0' NOT NULL,
-	ogg int(11) DEFAULT '0' NOT NULL,
-	h264 int(11) DEFAULT '0' NOT NULL,
-	ogv int(11) DEFAULT '0' NOT NULL,
-	web_m int(11) DEFAULT '0' NOT NULL,
-
-	l10n_diffsource mediumblob,
-	l10n_parent int(11) DEFAULT '0' NOT NULL,
-	sys_language_uid int(11) DEFAULT '0' NOT NULL,
-
-	PRIMARY KEY (uid),
-	KEY parent (pid)
+	mp3 tinyint(3) unsigned DEFAULT '0' NOT NULL,
+	ogg tinyint(3) unsigned DEFAULT '0' NOT NULL,
+	h264 tinyint(3) unsigned DEFAULT '0' NOT NULL,
+	ogv tinyint(3) unsigned DEFAULT '0' NOT NULL,
+	web_m tinyint(3) unsigned DEFAULT '0' NOT NULL
 );
