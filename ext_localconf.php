3b1f921c83f2d655c306cf99e97bfd3d72bda049
Alexander Stehlik <alexander.stehlik@gmail.com>
2019-03-10 19:58:15 +0100

    [TASK] Add ckeditor config for media description
    Reduce the available features in the same way as for
    htmlarea. Only allow minimal elements.

--- ext_localconf.php
+++ ext_localconf.php
@@ -21,6 +21,15 @@ defined('TYPO3_MODE') or die();
     . ' source="FILE: EXT:html5mediakit/Configuration/TSconfig/Page/mod.wizards.newContentElement.pagets">'
 );
 
+if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('rte_ckeditor')) {
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
+        '<INCLUDE_TYPOSCRIPT: source="FILE: EXT:html5mediakit/Configuration/TSconfig/Page/RTE.rte_ckeditor.pagets">'
+    );
+
+    $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['tx_html5mediakit_description'] =
+        'EXT:html5mediakit/Configuration/RTE/Description.yaml';
+}
+
 if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('rtehtmlarea')) {
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
         '<INCLUDE_TYPOSCRIPT: source="FILE: EXT:html5mediakit/Configuration/TSconfig/Page/RTE.rte_htmlarea.pagets">'
