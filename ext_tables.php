d832d3ed4ea630e0e362b68cee510bc3fb8c842c
Alexander Stehlik <alexander.stehlik@gmail.com>
2019-03-10 19:16:55 +0100

    [TASK] Adjust path to TCA description XLF file

--- ext_tables.php
+++ ext_tables.php
@@ -7,5 +7,5 @@ defined('TYPO3_MODE') or die();
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
     'tx_html5mediakit_domain_model_media',
-    'EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_csh_media.xml'
+    'EXT:html5mediakit/Resources/Private/Language/locallang_csh_media.xlf'
 );
