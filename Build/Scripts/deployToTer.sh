63f7dafd0d3b85407556aa2c2304ec54be534843
Alexander Stehlik <alexander.stehlik@gmail.com>
2019-03-10 21:00:07 +0100

    [TASK] Enable TER deployment in script

--- Build/Scripts/deployToTer.sh
+++ Build/Scripts/deployToTer.sh
@@ -1,7 +1,15 @@
-# Cleanup before we upload
+#!/usr/bin/env bash
+
+echo "Cleanup Git repository..."
 git reset --hard HEAD && git clean -fx
 
 TAG_MESSAGE=`git tag -n10 -l $TRAVIS_TAG | sed 's/^[0-9.]*[ ]*//g'`
-echo "Uploading release ${TRAVIS_TAG} to TER"
+echo "Extracted tag message: $TAG_MESSAGE"
 
-# .Build/bin/upload . "$TYPO3_ORG_USERNAME" "$TYPO3_ORG_PASSWORD" "$TAG_MESSAGE"
+echo "Renaming repository folder to match extension key..."
+cd ..
+mv typo3-extension-html5mediakit html5mediakit
+cd html5mediakit
+
+echo "Uploading release ${TRAVIS_TAG} to TER"
+./.Build/bin/upload . "$TYPO3_ORG_USERNAME" "$TYPO3_ORG_PASSWORD" "$TAG_MESSAGE"
