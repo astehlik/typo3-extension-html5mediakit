a706bfb74600aaf8016809e7bdddb23b428596d5
Alexander Stehlik <alexander.stehlik@gmail.com>
2019-03-10 16:38:06 +0100

    [!!!][TASK] Mark compatible with TYPO3 9
    Set TYPO3 core requirements in ext_emconf.php and composer.json.
    
    Fix license config in composer.json.
    
    Adjust test requirements and autoload config.

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,7 +13,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '2.0.0-dev',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.6.0-8.6.99',
+            'typo3' => '9.5.0-9.5.99',
             'extbase' => '',
         ],
         'conflicts' => [],
