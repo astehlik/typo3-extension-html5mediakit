f219278e96eaa41092f4c7f0e5bf13ac99e2742e
Alexander Stehlik <alexander.stehlik@gmail.com>
2019-03-10 16:41:20 +0100

    [TASK] Implement functional tests

---
+++ Tests/Functional/Controller/MediaController/AudioTest.php
@@ -0,0 +1,40 @@
+<?php
+/** @noinspection HtmlUnknownTarget */
+
+declare(strict_types=1);
+
+namespace Sto\Html5mediakit\Tests\Functional\Controller\MediaController;
+
+class AudioTest extends AbstractMediaControllerTest
+{
+    private $formats = [
+        'mpeg' => 'mp3',
+        'ogg' => 'ogg',
+    ];
+
+    public function testMediaControllerRendersAudio()
+    {
+        $responseBody = $this->loadFixturesAndGetResponseBody('audio');
+
+        $this->assertResponseContainsSources($responseBody);
+        $this->assertResponseContainsFallbackLinks($responseBody);
+        $this->assertContains('Testcaption', $responseBody);
+        $this->assertContains('Testdescription', $responseBody);
+    }
+
+    private function assertResponseContainsFallbackLinks(string $responseBody)
+    {
+        foreach ($this->formats as $extension) {
+            $expectedSource = sprintf('<a href="audio/media.%s">media.%1$s</a>', $extension);
+            $this->assertContains($expectedSource, $responseBody);
+        }
+    }
+
+    private function assertResponseContainsSources(string $responseBody)
+    {
+        foreach ($this->formats as $mimeType => $extension) {
+            $expectedSource = sprintf('<source src="audio/media.%s" type="audio/%s"/>', $extension, $mimeType);
+            $this->assertContains($expectedSource, $responseBody);
+        }
+    }
+}
---
+++ Tests/Functional/Controller/MediaController/VideoTest.php
@@ -0,0 +1,39 @@
+<?php
+declare(strict_types=1);
+
+namespace Sto\Html5mediakit\Tests\Functional\Controller\MediaController;
+
+class VideoTest extends AbstractMediaControllerTest
+{
+    private $formats = [
+        'webm' => 'webm',
+        'mp4' => 'mp4',
+        'ogg' => 'ogv',
+    ];
+
+    public function testMediaControllerShowsVideo()
+    {
+        $responseBody = $this->loadFixturesAndGetResponseBody('video');
+
+        $this->assertResponseContainsSources($responseBody);
+        $this->assertResponseContainsFallbackLinks($responseBody);
+        $this->assertContains('Testcaption', $responseBody);
+        $this->assertContains('Testdescription', $responseBody);
+    }
+
+    private function assertResponseContainsFallbackLinks(string $responseBody)
+    {
+        foreach ($this->formats as $extension) {
+            $expectedSource = sprintf('<a href="video/media.%s">media.%1$s</a>', $extension);
+            $this->assertContains($expectedSource, $responseBody);
+        }
+    }
+
+    private function assertResponseContainsSources(string $responseBody)
+    {
+        foreach ($this->formats as $mimeType => $extension) {
+            $expectedSource = sprintf('<source src="video/media.%s" type="video/%s"/>', $extension, $mimeType);
+            $this->assertContains($expectedSource, $responseBody);
+        }
+    }
+}
