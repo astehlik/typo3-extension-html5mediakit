bd5c4aeb07515fb9404ad96cc48d15580fbb1481
Alexander Stehlik <alexander.stehlik@gmail.com>
2019-03-30 10:57:41 +0100

    [BUGFIX] Fix version / release mixup in release script

--- Build/Scripts/release.sh
+++ Build/Scripts/release.sh
@@ -7,19 +7,19 @@ cd "$THIS_SCRIPT_DIR" || exit 1
 
 cd ../..
 
-version="$1"
-release=${version%.*}
+release="$1"
+version=${version%.*}
 
-if [[ -z "$version" ]]; then
-    echo "No version number provided!"
+if [[ -z "$release" ]]; then
+    echo "No release number provided!"
     exit 1
 fi
 
 echo "Preparing release for version $version"
 
-echo "Replacing version number in ext_emconf.php..."
+echo "Replacing release number in ext_emconf.php..."
 
-sed -i -E "s/'version' => '.+'/'version' => '$version'/" ext_emconf.php
+sed -i -E "s/'version' => '.+'/'version' => '$release'/" ext_emconf.php
 
 echo "Replacing version numbers documentation settings..."
 
