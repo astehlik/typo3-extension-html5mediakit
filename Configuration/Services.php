50bab09658f90ac9a8c5b5c9484512eb42c0c601
Alexander Stehlik <alex@stehlik-online.de>
2023-05-13 12:18:09 +0200

    [TASK] Configure services

---
+++ Configuration/Services.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+return function (ContainerConfigurator $configurator): void {
+    $configurator->services()
+        ->defaults()->autowire()->autoconfigure()->public()
+
+        ->load('Sto\\Html5mediakit\\Controller\\', __DIR__ . '/../Classes/Controller/')
+        ->load('Sto\\Html5mediakit\\Domain\\Repository\\', __DIR__ . '/../Classes/Domain/Repository/')
+        ->load('Sto\\Html5mediakit\\ViewHelpers\\', __DIR__ . '/../Classes/ViewHelpers/')
+    ;
+};
