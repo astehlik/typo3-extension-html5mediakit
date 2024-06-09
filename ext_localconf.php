e5a90db1536b07756185ca2a38767c9cd979528b
Alexander Stehlik <alex@stehlik-online.de>
2024-06-09 21:16:46 +0200

    [!!!][TASK] Move new content wizard TSconfig
    The config is moved to the new default location
    Configuration/page.tsconfig where it is automatically loaded by TYPO3.
    The deprecated call to addPageTSConfig() is removed.

--- ext_localconf.php
+++ ext_localconf.php
@@ -19,11 +19,6 @@ defined('TYPO3') or die();
     []
 );
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
-    '<INCLUDE_TYPOSCRIPT:'
-    . ' source="FILE: EXT:html5mediakit/Configuration/TSconfig/Page/mod.wizards.newContentElement.pagets">'
-);
-
 if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('rte_ckeditor')) {
     $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['tx_html5mediakit_description'] =
         'EXT:html5mediakit/Configuration/RTE/Description.yaml';
