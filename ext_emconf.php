b1f7d26c48e0e38957388344926182db2d95996a
Alexander Stehlik <alex@stehlik-online.de>
2024-06-09 23:25:07 +0200

    [TASK] Update Composer dependencies, add PHPStan, move bin

--- ext_emconf.php
+++ ext_emconf.php
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '12.1.1',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.4.0-12.4.99',
+            'typo3' => '13.1.0-13.1.99',
             'extbase' => '',
         ],
         'conflicts' => [],
