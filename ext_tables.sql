dab4a00491925ea739aab5bf7dc5964abfd44aaa
Alexander Stehlik <astehlik@users.noreply.github.com>
2025-08-04 13:16:20 +0000

    [CLENAUP] Remove obsolete entries from ext_tables.sql (#23)

--- ext_tables.sql
+++ ext_tables.sql
@@ -1,11 +1,4 @@
 #
-# Table structure for table 'tt_content'
-#
-CREATE TABLE tt_content (
-	tx_html5mediakit_media int(11) DEFAULT '0' NOT NULL
-);
-
-#
 # Table structure for table 'sys_file_reference'
 #
 CREATE TABLE sys_file_reference (
@@ -23,13 +16,4 @@ CREATE TABLE tx_html5mediakit_domain_model_media (
 	parent_table varchar(255) DEFAULT '' NOT NULL,
 
 	type varchar(100) DEFAULT '' NOT NULL,
-	tracks tinyint(3) unsigned DEFAULT '0' NOT NULL,
-	caption varchar(255) DEFAULT '' NOT NULL,
-	description text NOT NULL,
-	mp3 tinyint(3) unsigned DEFAULT '0' NOT NULL,
-	ogg tinyint(3) unsigned DEFAULT '0' NOT NULL,
-	h264 tinyint(3) unsigned DEFAULT '0' NOT NULL,
-	ogv tinyint(3) unsigned DEFAULT '0' NOT NULL,
-	poster tinyint(3) unsigned DEFAULT '0' NOT NULL,
-	web_m tinyint(3) unsigned DEFAULT '0' NOT NULL
 );
